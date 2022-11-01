f1ed415986a5da0831ad526dba9e078fd02c3c05
LAPTOP-DSGKT2L5\88693 <amo6324@yahoo.com.tw>
2022-11-01 22:00:08 +0800

    新增登入頁面按鈕

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React from "react";
 
 export default class Login extends React.Component {
-
-    render() {
-        return <h1>Hello, New Page</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              帳號
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            ></input>
+            <div id="emailHelp" class="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">
+              密碼
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            ></input>
+          </div>
+          <div class="mb-3 form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="exampleCheck1"
+            ></input>
+            <label class="form-check-label" for="exampleCheck1">
+              記住我
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary">
+            登入
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
